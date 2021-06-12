81603f7d9cc051d642b864146347f953fecb8b3e
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2021-06-12 15:33:19 -0700

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import { join } from 'path';
 import { cwd } from 'process';
-import staticAdapter from '@sveltejs/adapter-static';
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +14,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: staticAdapter()
+		adapter: netlify()
 	}
 };
 
