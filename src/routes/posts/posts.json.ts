f830bb2dff60ae694e30859e38bfa219f904fe51
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2021-06-12 15:44:30 -0700

    fix issue

--- src/routes/posts/posts.json.ts
+++ src/routes/posts/posts.json.ts
@@ -2,5 +2,5 @@ import { getAllPosts } from '../../lib/blog';
 
 export async function get() {
 	//return { body: await getAllPosts() };
-	return { body: ['1-dry-css-interactive-elements.svx'] };
+	return { body: ['1-dry-css-interactive-elements'] };
 }
